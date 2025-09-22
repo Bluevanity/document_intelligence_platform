8a3e544e52e3d36722da38349ec6802a9ea82da2
hp <diamonfnwachi40@gmail.com>
2025-09-22 14:54:16 +0100

    add openCV for image pre-processing

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,15 @@
 import pytesseract
 from PIL import Image
+import cv2
+
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
-text = pytesseract.image_to_string(Image.open('Screenshot (226).png'))
+
+img_cv = cv2.imread('Screenshot (226).png')
+img_rgb = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
+
+text = pytesseract.image_to_string(img_rgb)
 print(text)
+
+# Everything seems to work pretty much the same even with image pre-processing.
+# Perhaps this will make a difference with more data or images with more text. 
